54cfd69d3a619eebe8c570f77a97c8c951243842
maryam <maryam.raeisi@specific-group.com>
2024-05-23 02:05:49 +0330

    Initial commit

---
+++ src/main/java/Account.java
@@ -0,0 +1,13 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Account {
+    private String firstName;
+    private String lastName;
+    private byte age;
+    private String telegramId;
+    private long phoneNumber;
+    private String location;
+}
